29818a1628ac8271aaa6da89cbfb6590253ebf20
unknown <asifpashraf@gmail.com>
2024-11-10 20:36:20 +0530

    ci

--- src/components/Latest_movies.jsx
+++ src/components/Latest_movies.jsx
@@ -44,7 +44,7 @@ function Latest_movies() {
     
 
   return (
-    <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+    <div className='p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
 
       
         {data.results.map((movieCard,index)=>{
